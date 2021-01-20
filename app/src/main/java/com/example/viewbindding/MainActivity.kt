157ada3987440776e749076306c240131184ffb3
athoriq1224 <athoriq2004@gmail.com>
2021-01-20 09:20:03 +0700

    View Binding

---
+++ app/src/main/java/com/example/viewbindding/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.viewbindding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.viewbindding.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnSend.setOnClickListener{
+            val data = edit_text.text
+            saveData(data.toString())
+        }
+
+    }
+    private fun saveData(data: String){
+        binding.textView.text = data
+    }
+
+}
